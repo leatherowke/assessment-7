eb77b96be73c57a76e7dab88cc03aecee15d8fe2
Kegan Leatherow <leatherowke@gmail.com>
2022-07-22 11:38:59 -0500

    done

--- newFunctions.js
+++ newFunctions.js
@@ -1,24 +1,61 @@
 function addToZero (arr) {
+   
     for(let i=0; i<arr.length; i++){
-        for(let j=1; j<arr.length; j++){ 
-            if(i+j === 0){
-                return true
+        for(let j = i + 1; j<arr.length; j++){ 
+            
+            // console.log(num)
+            if( arr[i] + arr[j] === 0){
+                answer = 'true'
+                return answer
             }
             else{
-                return false
+                answer = 'false'
+                return answer
             }
         }
     }
+   
 };
 
 
-function uniqueChar (arr){
-for( let i = 0; i<arr.length; i++){
-    if (i != '!@#$%^&*'){
-        return false
+console.log(addToZero([1,-1]))
+
+
+
+
+function uniqueChar(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
     }
-    else {
-        return true
+    return uniqueChars.size === word.length
+  }
+
+console.log(uniqueChar('monday'))
+
+function pangram (arr) {
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      if(strArr.indexOf(alphabet[i]) < 0){
+        return false;
+      }
     }
+    return true;
+  }
+
+  function longestWord (str){
+let strSplit = str.split(' ');
+let longestWord = 0;
+
+for(let i = 0; i < strSplit.length; i++){
+    if(strSplit[i] > longestWord){
+        longestWord = strSplit[i]
+    }
+}  
+return longestWord
 }
-};
+// this will have a short run time when compared to the others due to not running a for loop inside another loop
+
+console.log(longestWord('hi','hello'))
